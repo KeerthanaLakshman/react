00954f6d16757917269eca4ffda0ee8fddabeffb
KeerthanaLakshman <keerthanalakshman15@gmail.com>
2023-07-05 18:08:09 +0530

    spotify

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import im from "../assets/download.png"
-
+import './Home.css'
+import im from "../assets/download1.png"
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
-    <div>Home</div>
-    <div className='image'>
-        <img src={im} alt="" />
-        <div className='abc'>Helloo</div>
+    <div className='spotify'>
+      <div className='image'><img src={im} alt="" /></div>
+      <div className='A'>Spotify</div>
+      <div className='B'>Premium</div>
+      <div className='C'>Help</div>
+      <div className='D'>Download</div>
+      <div className='G'>|</div>
+      <div className='E'>Sign up</div>
+      <Link to={"/Login"} id='log'>
+        <div className="F">Log in</div>
+      </Link>
+  </div>
+    <div className='main'>
+      <div className='H'><b>Music for everyone.</b></div>
+      <div className='I'>Millions of songs.No credit card needed.</div>
+      <button>GET SPOTIFY FREE</button>
     </div>
     </>
   )
 }
-
 export default Home
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
   return (
     <>
-    <div>Login</div>
-    <img src="" alt="" />
+    <div className='ab'>
+      <div className='a'>LOGIN</div><br />
+      <form id='log'>
+        <label htmlFor="email">Email:</label><br />
+        <input type="text"  placeholder='Enter email' id='email'/><br /><br />
+        <label htmlFor="psw">Password:</label><br />
+        <input type="password" placeholder='Enter password' id='psw'/><br /><br />
+        <input type="checkbox" name="check" />
+        <label htmlFor="show">Show password</label><br />
+        <button className='button' id='button'>SIGN IN</button><br /><br /><br />
+        <div className='zx'>Forgot  <a href="none"> Username / Password ?</a></div><br />
+        <div className='xy'>Don't have an account? <a href="">Sign Up</a></div>
+      </form>
+      
+    </div>
+    
     </>
   )
 }
